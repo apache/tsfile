fd6978ee21e46e1ce7d4e57b6913823708ce8db4
Jialin Qiao <qjl16@mails.tsinghua.edu.cn>
2019-12-26 11:30:10 +0800

    [IOTDB-386] Vectorize the raw data query process (#652)
    * add batch reader interfaces
    
    * modify docs in SeriesReaderWithoutValueFilter
    
    * Add data to TSExecuteStatementResp (#631)
    
    * add fill buffer in EngineDataSetWithoutValueFilter (#646)
    
    * [IOTDB-330] Improve the reading method of unsequence data (#619)
    
    * change UnseqResourceMergeReader to IBatchReader
    
    * Fix bug of "Has not execute query" error when querying (#656)
    
    * Original query process (#653)
    
    * add aggregation reader
    
    * Update SeriesReaderWithoutValueFilter.java
    
    * fix code-coverage stage does not compile service-rpc and some other modules
    
    Co-authored-by: Jackie Tien <JackieTien@foxmail.com>
    Co-authored-by: Dawei Liu <atoildw@163.com>
    Co-authored-by: Zesong Sun <szs19@mails.tsinghua.edu.cn>
    Co-authored-by: Lei Rui <33376433+LeiRui@users.noreply.github.com>

--- src/main/java/org/apache/iotdb/tsfile/read/reader/IAggregateReader.java
+++ src/main/java/org/apache/iotdb/tsfile/read/reader/IAggregateReader.java
@@ -16,20 +16,20 @@
  * specific language governing permissions and limitations
  * under the License.
  */
-package org.apache.iotdb.tsfile.read.reader.chunk;
+package org.apache.iotdb.tsfile.read.reader;
 
+import java.io.IOException;
 import org.apache.iotdb.tsfile.file.header.PageHeader;
-import org.apache.iotdb.tsfile.read.common.Chunk;
 
-public class ChunkReaderWithoutFilter extends ChunkReader {
+public interface IAggregateReader extends IBatchReader {
 
-  public ChunkReaderWithoutFilter(Chunk chunk) {
-    super(chunk);
-  }
+  /**
+   * Returns meta-information of batch data.
+   * <p>
+   * Returns null if batch data comes from memory. Returns pageHeader if batch data comes from page
+   * data.
+   */
+  PageHeader nextPageHeader() throws IOException;
 
-  @Override
-  public boolean pageSatisfied(PageHeader pageHeader) {
-    return pageHeader.getEndTime() > deletedAt;
-  }
-
+  void skipPageData() throws IOException;
 }
