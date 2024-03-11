e2562fabf2394dfb9a33980c2e2f338be319ae31
Caideyipi <87789683+Caideyipi@users.noreply.github.com>
2024-03-11 10:36:18 +0800

    Update TabletTest.java

--- tsfile/src/test/java/org/apache/tsfile/write/record/TabletTest.java
+++ tsfile/src/test/java/org/apache/tsfile/write/record/TabletTest.java
@@ -31,7 +31,6 @@ import java.util.ArrayList;
 import java.util.List;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
 
 public class TabletTest {
