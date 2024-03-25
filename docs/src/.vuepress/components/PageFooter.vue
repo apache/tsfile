d693267bd15d714fa741f6a6c0c42675de6e350b
CritasWang <critas@outlook.com>
2024-03-25 10:22:06 +0800

    add matomo

--- docs/src/.vuepress/components/PageFooter.vue
+++ docs/src/.vuepress/components/PageFooter.vue
@@ -23,9 +23,6 @@
       Apache and the Apache feather logo are trademarks of The Apache Software Foundation</p>
     <p style="text-align: center; margin-top: 10px; color: #909399; font-size: 12px; margin: 0 30px;">
       <strong>Have a question?</strong> Connect with us on issues. </p>
-    <p style="text-align: center; margin-top: 10px; color: #909399; font-size: 12px; margin: 0 30px;">
-      We use <a href="https://analytics.google.com">Google Analytics</a> to collect anonymous, aggregated usage information.
-    </p>
   </footer>
 </template>
 <script setup lang="ts">
