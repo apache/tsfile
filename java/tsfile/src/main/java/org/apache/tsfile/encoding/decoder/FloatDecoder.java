a264c5f735994b5b3b71f4e2e55d6fa467a216a4
gzh0806 <149436650+gzh23@users.noreply.github.com>
2024-07-02 11:27:05 +0800

    fix rlbe (#143)

--- java/tsfile/src/main/java/org/apache/tsfile/encoding/decoder/FloatDecoder.java
+++ java/tsfile/src/main/java/org/apache/tsfile/encoding/decoder/FloatDecoder.java
@@ -71,6 +71,17 @@ public class FloatDecoder extends Decoder {
         throw new TsFileDecodingException(
             String.format("data type %s is not supported by FloatDecoder", dataType));
       }
+    } else if (encodingType == TSEncoding.RLBE) {
+      if (dataType == TSDataType.FLOAT) {
+        decoder = new IntRLBEDecoder();
+        logger.debug("tsfile-encoding FloatDecoder: init decoder using int-rlbe and float");
+      } else if (dataType == TSDataType.DOUBLE) {
+        decoder = new LongRLBEDecoder();
+        logger.debug("tsfile-encoding FloatDecoder: init decoder using long-rlbe and double");
+      } else {
+        throw new TsFileDecodingException(
+                String.format("data type %s is not supported by FloatDecoder", dataType));
+      }
     } else {
       throw new TsFileDecodingException(
           String.format("%s encoding is not supported by FloatDecoder", encodingType));
