0ae8ddabf632d6c130d86a51fe5654af0d66b75d
shuwenwei <55970239+shuwenwei@users.noreply.github.com>
2024-07-05 17:51:21 +0800

    support separated aligned chunk (#151)

--- java/tsfile/src/main/java/org/apache/tsfile/read/reader/page/AlignedPageReader.java
+++ java/tsfile/src/main/java/org/apache/tsfile/read/reader/page/AlignedPageReader.java
@@ -469,4 +469,12 @@ public class AlignedPageReader implements IPageReader {
       builder = new TsBlockBuilder((int) timePageReader.getStatistics().getCount(), dataTypes);
     }
   }
+
+  public TimePageReader getTimePageReader() {
+    return timePageReader;
+  }
+
+  public List<ValuePageReader> getValuePageReaderList() {
+    return valuePageReaderList;
+  }
 }
