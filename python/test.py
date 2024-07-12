a86e14ed99888d109ca0b4a04081ef3dad22febc
HTHou <hhaonan@outlook.com>
2024-07-12 17:02:24 +0800

    enable test

--- python/test.py
+++ python/test.py
@@ -25,12 +25,6 @@ import unittest as ut
 import numpy as np
 import pandas as pd
 
-if platform.system() == "Windows":
-    extra_dll_dir = os.path.join(os.path.dirname(__file__), "tsfile")
-    os.add_dll_directory(extra_dll_dir)
-    print(extra_dll_dir)
-    print(glob.glob(extra_dll_dir + "/*"))
-
 import tsfile as ts
 from tsfile.tsfile import EmptyFileError
 
