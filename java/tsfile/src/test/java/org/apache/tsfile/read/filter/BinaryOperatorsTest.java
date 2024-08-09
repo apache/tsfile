b78cf57ba954a2f1591324a047558d0d5c96616a
FearfulTomcat27 <1471335448@qq.com>
2024-08-09 20:52:24 +0800

    save for pr

--- java/tsfile/src/test/java/org/apache/tsfile/read/filter/BinaryOperatorsTest.java
+++ java/tsfile/src/test/java/org/apache/tsfile/read/filter/BinaryOperatorsTest.java
@@ -14,18 +14,20 @@
 
 package org.apache.tsfile.read.filter;
 
+import org.apache.tsfile.read.filter.basic.Filter;
+import org.apache.tsfile.read.filter.factory.ValueFilterApi;
+import org.apache.tsfile.utils.Binary;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.HashSet;
+
 import static org.apache.tsfile.common.conf.TSFileConfig.STRING_CHARSET;
 import static org.apache.tsfile.enums.TSDataType.TEXT;
 import static org.apache.tsfile.read.filter.FilterTestUtil.DEFAULT_TIMESTAMP;
 import static org.apache.tsfile.read.filter.factory.ValueFilterApi.DEFAULT_MEASUREMENT_INDEX;
-
-import java.util.Arrays;
-import java.util.HashSet;
-import org.apache.tsfile.read.filter.basic.Filter;
-import org.apache.tsfile.read.filter.factory.ValueFilterApi;
-import org.apache.tsfile.utils.Binary;
-import org.junit.Assert;
-import org.junit.Test;
 
 public class BinaryOperatorsTest {
 
@@ -33,58 +35,47 @@ public class BinaryOperatorsTest {
   public void testEq() {
     Filter eq = ValueFilterApi.eq(DEFAULT_MEASUREMENT_INDEX, new Binary("a", STRING_CHARSET), TEXT);
     Assert.assertTrue(eq.satisfyBinary(DEFAULT_TIMESTAMP, new Binary("a", STRING_CHARSET)));
-    Assert.assertFalse(
-        eq.satisfyBinary(DEFAULT_TIMESTAMP, new Binary("b", STRING_CHARSET)));
+    Assert.assertFalse(eq.satisfyBinary(DEFAULT_TIMESTAMP, new Binary("b", STRING_CHARSET)));
   }
 
   @Test
   public void testNotEq() {
     Filter notEq =
         ValueFilterApi.notEq(DEFAULT_MEASUREMENT_INDEX, new Binary("a", STRING_CHARSET), TEXT);
-    Assert.assertTrue(
-        notEq.satisfyBinary(DEFAULT_TIMESTAMP, new Binary("b", STRING_CHARSET)));
-    Assert.assertFalse(
-        notEq.satisfyBinary(DEFAULT_TIMESTAMP, new Binary("a", STRING_CHARSET)));
+    Assert.assertTrue(notEq.satisfyBinary(DEFAULT_TIMESTAMP, new Binary("b", STRING_CHARSET)));
+    Assert.assertFalse(notEq.satisfyBinary(DEFAULT_TIMESTAMP, new Binary("a", STRING_CHARSET)));
   }
 
   @Test
   public void testGt() {
     Filter gt = ValueFilterApi.gt(DEFAULT_MEASUREMENT_INDEX, new Binary("a", STRING_CHARSET), TEXT);
     Assert.assertTrue(gt.satisfyBinary(DEFAULT_TIMESTAMP, new Binary("b", STRING_CHARSET)));
-    Assert.assertFalse(
-        gt.satisfyBinary(DEFAULT_TIMESTAMP, new Binary("a", STRING_CHARSET)));
+    Assert.assertFalse(gt.satisfyBinary(DEFAULT_TIMESTAMP, new Binary("a", STRING_CHARSET)));
   }
 
   @Test
   public void testGtEq() {
     Filter gtEq =
         ValueFilterApi.gtEq(DEFAULT_MEASUREMENT_INDEX, new Binary("a", STRING_CHARSET), TEXT);
-    Assert.assertTrue(
-        gtEq.satisfyBinary(DEFAULT_TIMESTAMP, new Binary("a", STRING_CHARSET)));
-    Assert.assertTrue(
-        gtEq.satisfyBinary(DEFAULT_TIMESTAMP, new Binary("b", STRING_CHARSET)));
-    Assert.assertFalse(
-        gtEq.satisfyBinary(DEFAULT_TIMESTAMP, new Binary("c", STRING_CHARSET)));
+    Assert.assertTrue(gtEq.satisfyBinary(DEFAULT_TIMESTAMP, new Binary("a", STRING_CHARSET)));
+    Assert.assertTrue(gtEq.satisfyBinary(DEFAULT_TIMESTAMP, new Binary("b", STRING_CHARSET)));
+    Assert.assertTrue(gtEq.satisfyBinary(DEFAULT_TIMESTAMP, new Binary("c", STRING_CHARSET)));
   }
 
   @Test
   public void testLt() {
     Filter lt = ValueFilterApi.lt(DEFAULT_MEASUREMENT_INDEX, new Binary("b", STRING_CHARSET), TEXT);
     Assert.assertTrue(lt.satisfyBinary(DEFAULT_TIMESTAMP, new Binary("a", STRING_CHARSET)));
-    Assert.assertFalse(
-        lt.satisfyBinary(DEFAULT_TIMESTAMP, new Binary("b", STRING_CHARSET)));
+    Assert.assertFalse(lt.satisfyBinary(DEFAULT_TIMESTAMP, new Binary("b", STRING_CHARSET)));
   }
 
   @Test
   public void testLtEq() {
     Filter ltEq =
         ValueFilterApi.ltEq(DEFAULT_MEASUREMENT_INDEX, new Binary("b", STRING_CHARSET), TEXT);
-    Assert.assertTrue(
-        ltEq.satisfyBinary(DEFAULT_TIMESTAMP, new Binary("a", STRING_CHARSET)));
-    Assert.assertTrue(
-        ltEq.satisfyBinary(DEFAULT_TIMESTAMP, new Binary("b", STRING_CHARSET)));
-    Assert.assertFalse(
-        ltEq.satisfyBinary(DEFAULT_TIMESTAMP, new Binary("c", STRING_CHARSET)));
+    Assert.assertTrue(ltEq.satisfyBinary(DEFAULT_TIMESTAMP, new Binary("a", STRING_CHARSET)));
+    Assert.assertTrue(ltEq.satisfyBinary(DEFAULT_TIMESTAMP, new Binary("b", STRING_CHARSET)));
+    Assert.assertFalse(ltEq.satisfyBinary(DEFAULT_TIMESTAMP, new Binary("c", STRING_CHARSET)));
   }
 
   @Test
@@ -95,10 +86,8 @@ public class BinaryOperatorsTest {
             new Binary("a", STRING_CHARSET),
             new Binary("c", STRING_CHARSET),
             TEXT);
-    Assert.assertTrue(
-        between.satisfyBinary(DEFAULT_TIMESTAMP, new Binary("b", STRING_CHARSET)));
-    Assert.assertFalse(
-        between.satisfyBinary(DEFAULT_TIMESTAMP, new Binary("d", STRING_CHARSET)));
+    Assert.assertTrue(between.satisfyBinary(DEFAULT_TIMESTAMP, new Binary("b", STRING_CHARSET)));
+    Assert.assertFalse(between.satisfyBinary(DEFAULT_TIMESTAMP, new Binary("d", STRING_CHARSET)));
   }
 
   @Test
@@ -109,8 +98,7 @@ public class BinaryOperatorsTest {
             new Binary("a", STRING_CHARSET),
             new Binary("c", STRING_CHARSET),
             TEXT);
-    Assert.assertTrue(
-        notBetween.satisfyBinary(DEFAULT_TIMESTAMP, new Binary("d", STRING_CHARSET)));
+    Assert.assertTrue(notBetween.satisfyBinary(DEFAULT_TIMESTAMP, new Binary("d", STRING_CHARSET)));
     Assert.assertFalse(
         notBetween.satisfyBinary(DEFAULT_TIMESTAMP, new Binary("b", STRING_CHARSET)));
   }
@@ -124,8 +112,7 @@ public class BinaryOperatorsTest {
                 Arrays.asList(new Binary("a", STRING_CHARSET), new Binary("b", STRING_CHARSET))),
             TEXT);
     Assert.assertTrue(in.satisfyBinary(DEFAULT_TIMESTAMP, new Binary("a", STRING_CHARSET)));
-    Assert.assertFalse(
-        in.satisfyBinary(DEFAULT_TIMESTAMP, new Binary("c", STRING_CHARSET)));
+    Assert.assertFalse(in.satisfyBinary(DEFAULT_TIMESTAMP, new Binary("c", STRING_CHARSET)));
   }
 
   @Test
@@ -137,17 +124,14 @@ public class BinaryOperatorsTest {
                 Arrays.asList(new Binary("a", STRING_CHARSET), new Binary("b", STRING_CHARSET))),
             TEXT);
     Assert.assertTrue(in.satisfyBinary(DEFAULT_TIMESTAMP, new Binary("c", STRING_CHARSET)));
-    Assert.assertFalse(
-        in.satisfyBinary(DEFAULT_TIMESTAMP, new Binary("a", STRING_CHARSET)));
+    Assert.assertFalse(in.satisfyBinary(DEFAULT_TIMESTAMP, new Binary("a", STRING_CHARSET)));
   }
 
   @Test
   public void testRegexp() {
     Filter regexp = ValueFilterApi.regexp(DEFAULT_MEASUREMENT_INDEX, "a.*", TEXT);
-    Assert.assertTrue(
-        regexp.satisfyBinary(DEFAULT_TIMESTAMP, new Binary("abc", STRING_CHARSET)));
-    Assert.assertFalse(
-        regexp.satisfyBinary(DEFAULT_TIMESTAMP, new Binary("bcd", STRING_CHARSET)));
+    Assert.assertTrue(regexp.satisfyBinary(DEFAULT_TIMESTAMP, new Binary("abc", STRING_CHARSET)));
+    Assert.assertFalse(regexp.satisfyBinary(DEFAULT_TIMESTAMP, new Binary("bcd", STRING_CHARSET)));
   }
 
   @Test
@@ -158,4 +142,7 @@ public class BinaryOperatorsTest {
     Assert.assertFalse(
         notRegexp.satisfyBinary(DEFAULT_TIMESTAMP, new Binary("abc", STRING_CHARSET)));
   }
+
+  @Test
+  public void testStatistics() {}
 }
--- java/tsfile/src/test/java/org/apache/tsfile/read/filter/StatisticsFilterTest.java
+++ java/tsfile/src/test/java/org/apache/tsfile/read/filter/StatisticsFilterTest.java
@@ -21,19 +21,23 @@ package org.apache.tsfile.read.filter;
 import org.apache.tsfile.enums.TSDataType;
 import org.apache.tsfile.file.metadata.IMetadata;
 import org.apache.tsfile.file.metadata.statistics.LongStatistics;
+import org.apache.tsfile.file.metadata.statistics.StringStatistics;
 import org.apache.tsfile.file.metadata.statistics.TimeStatistics;
 import org.apache.tsfile.read.filter.basic.Filter;
 import org.apache.tsfile.read.filter.factory.FilterFactory;
 import org.apache.tsfile.read.filter.factory.TimeFilterApi;
 import org.apache.tsfile.read.filter.factory.ValueFilterApi;
+import org.apache.tsfile.utils.Binary;
 
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Set;
 
+import static org.apache.tsfile.common.conf.TSFileConfig.STRING_CHARSET;
 import static org.apache.tsfile.read.filter.FilterTestUtil.newAlignedMetadata;
 import static org.apache.tsfile.read.filter.FilterTestUtil.newMetadata;
 import static org.apache.tsfile.read.filter.factory.ValueFilterApi.DEFAULT_MEASUREMENT_INDEX;
@@ -45,10 +49,15 @@ public class StatisticsFilterTest {
   private IMetadata metadata1;
   private IMetadata metadata2;
   private IMetadata metadata3;
+  private IMetadata metadata4;
+  private IMetadata metadata5;
+  private IMetadata metadata6;
 
   private IMetadata alignedMetadata1;
   private IMetadata alignedMetadata2;
   private IMetadata alignedMetadata3;
+  private IMetadata alignedMetadata4;
+  private IMetadata alignedMetadata5;
 
   @Before
   public void before() {
@@ -72,13 +81,31 @@ public class StatisticsFilterTest {
     LongStatistics statistic3 = new LongStatistics();
     statistic3.update(10L, 10L);
 
+    StringStatistics statistic4 = new StringStatistics();
+    statistic4.update(100L, new Binary("a", STRING_CHARSET));
+    statistic4.update(200L, new Binary("a", STRING_CHARSET));
+
+    StringStatistics statistic5 = new StringStatistics();
+    statistic5.update(100L, new Binary("b", STRING_CHARSET));
+    statistic5.update(200L, new Binary("b", STRING_CHARSET));
+
+    StringStatistics statistic6 = new StringStatistics();
+    statistic6.update(100L, new Binary("a", STRING_CHARSET));
+    statistic6.update(200L, new Binary("b", STRING_CHARSET));
+
     metadata1 = newMetadata(statistic1);
     metadata2 = newMetadata(statistic2);
     metadata3 = newMetadata(statistic3);
+    metadata4 = newMetadata(statistic4);
+    metadata5 = newMetadata(statistic5);
+    metadata6 = newMetadata(statistic6);
 
     alignedMetadata1 = newAlignedMetadata(timeStatistics1, statistic1);
     alignedMetadata2 = newAlignedMetadata(timeStatistics2, statistic2);
     alignedMetadata3 = newAlignedMetadata(timeStatistics1, null);
+
+    alignedMetadata4 = newAlignedMetadata(timeStatistics1, statistic4);
+    alignedMetadata5 = newAlignedMetadata(timeStatistics1, statistic5);
   }
 
   @Test
@@ -96,6 +123,17 @@ public class StatisticsFilterTest {
     Assert.assertFalse(valueEq.allSatisfy(metadata1));
     Assert.assertFalse(valueEq.allSatisfy(metadata2));
     Assert.assertFalse(valueEq.allSatisfy(metadata3));
+
+    Filter stringEq =
+        ValueFilterApi.eq(
+            DEFAULT_MEASUREMENT_INDEX, new Binary("a", STRING_CHARSET), TSDataType.STRING);
+    Assert.assertFalse(stringEq.canSkip(metadata4));
+    Assert.assertTrue(stringEq.canSkip(metadata5));
+    Assert.assertFalse(stringEq.canSkip(metadata6));
+
+    Assert.assertTrue(stringEq.allSatisfy(metadata4));
+    Assert.assertFalse(stringEq.allSatisfy(metadata5));
+    Assert.assertFalse(stringEq.allSatisfy(metadata6));
   }
 
   @Test
@@ -113,6 +151,17 @@ public class StatisticsFilterTest {
     Assert.assertTrue(valueNotEq.allSatisfy(metadata1));
     Assert.assertFalse(valueNotEq.allSatisfy(metadata2));
     Assert.assertTrue(valueNotEq.allSatisfy(metadata3));
+
+    Filter stringNotEq =
+        ValueFilterApi.notEq(
+            DEFAULT_MEASUREMENT_INDEX, new Binary("a", STRING_CHARSET), TSDataType.STRING);
+    Assert.assertTrue(stringNotEq.canSkip(metadata4));
+    Assert.assertFalse(stringNotEq.canSkip(metadata5));
+    Assert.assertFalse(stringNotEq.canSkip(metadata6));
+
+    Assert.assertFalse(stringNotEq.allSatisfy(metadata4));
+    Assert.assertTrue(stringNotEq.allSatisfy(metadata5));
+    Assert.assertFalse(stringNotEq.allSatisfy(metadata6));
   }
 
   @Test
@@ -128,6 +177,17 @@ public class StatisticsFilterTest {
     Assert.assertFalse(valueGt.canSkip(metadata2));
     Assert.assertFalse(valueGt.allSatisfy(metadata1));
     Assert.assertTrue(valueGt.allSatisfy(metadata2));
+
+    Filter stringGt =
+        ValueFilterApi.gt(
+            DEFAULT_MEASUREMENT_INDEX, new Binary("a", STRING_CHARSET), TSDataType.STRING);
+    Assert.assertTrue(stringGt.canSkip(metadata4));
+    Assert.assertFalse(stringGt.canSkip(metadata5));
+    Assert.assertFalse(stringGt.canSkip(metadata6));
+
+    Assert.assertFalse(stringGt.allSatisfy(metadata4));
+    Assert.assertTrue(stringGt.allSatisfy(metadata5));
+    Assert.assertFalse(stringGt.allSatisfy(metadata6));
   }
 
   @Test
@@ -143,6 +203,17 @@ public class StatisticsFilterTest {
     Assert.assertFalse(valueGtEq.canSkip(metadata2));
     Assert.assertFalse(valueGtEq.allSatisfy(metadata1));
     Assert.assertTrue(valueGtEq.allSatisfy(metadata2));
+
+    Filter stringGtEq =
+        ValueFilterApi.gtEq(
+            DEFAULT_MEASUREMENT_INDEX, new Binary("a", STRING_CHARSET), TSDataType.STRING);
+    Assert.assertFalse(stringGtEq.canSkip(metadata4));
+    Assert.assertFalse(stringGtEq.canSkip(metadata5));
+    Assert.assertFalse(stringGtEq.canSkip(metadata6));
+
+    Assert.assertTrue(stringGtEq.allSatisfy(metadata4));
+    Assert.assertTrue(stringGtEq.allSatisfy(metadata5));
+    Assert.assertTrue(stringGtEq.allSatisfy(metadata6));
   }
 
   @Test
@@ -158,6 +229,17 @@ public class StatisticsFilterTest {
     Assert.assertTrue(valueLt.canSkip(metadata2));
     Assert.assertFalse(valueLt.allSatisfy(metadata1));
     Assert.assertFalse(valueLt.allSatisfy(metadata2));
+
+    Filter stringLt =
+        ValueFilterApi.lt(
+            DEFAULT_MEASUREMENT_INDEX, new Binary("b", STRING_CHARSET), TSDataType.STRING);
+    Assert.assertFalse(stringLt.canSkip(metadata4));
+    Assert.assertTrue(stringLt.canSkip(metadata5));
+    Assert.assertFalse(stringLt.canSkip(metadata6));
+
+    Assert.assertTrue(stringLt.allSatisfy(metadata4));
+    Assert.assertFalse(stringLt.allSatisfy(metadata5));
+    Assert.assertFalse(stringLt.allSatisfy(metadata6));
   }
 
   @Test
@@ -171,6 +253,17 @@ public class StatisticsFilterTest {
     Assert.assertTrue(valueLtEq.canSkip(metadata2));
     Assert.assertFalse(valueLtEq.allSatisfy(metadata1));
     Assert.assertFalse(valueLtEq.allSatisfy(metadata2));
+
+    Filter stringLtEq =
+        ValueFilterApi.ltEq(
+            DEFAULT_MEASUREMENT_INDEX, new Binary("a", STRING_CHARSET), TSDataType.STRING);
+    Assert.assertFalse(stringLtEq.canSkip(metadata4));
+    Assert.assertTrue(stringLtEq.canSkip(metadata5));
+    Assert.assertFalse(stringLtEq.canSkip(metadata6));
+
+    Assert.assertTrue(stringLtEq.allSatisfy(metadata4));
+    Assert.assertFalse(stringLtEq.allSatisfy(metadata5));
+    Assert.assertFalse(stringLtEq.allSatisfy(metadata6));
   }
 
   @Test
@@ -237,6 +330,20 @@ public class StatisticsFilterTest {
     Assert.assertFalse(valueBetweenAnd.allSatisfy(metadata1));
     Assert.assertFalse(valueBetweenAnd.allSatisfy(metadata2));
     Assert.assertTrue(valueBetweenAnd.allSatisfy(metadata3));
+
+    Filter stringBetweenAnd =
+        ValueFilterApi.between(
+            DEFAULT_MEASUREMENT_INDEX,
+            new Binary("a", STRING_CHARSET),
+            new Binary("b", STRING_CHARSET),
+            TSDataType.STRING);
+    Assert.assertFalse(stringBetweenAnd.canSkip(metadata4));
+    Assert.assertFalse(stringBetweenAnd.canSkip(metadata5));
+    Assert.assertFalse(stringBetweenAnd.canSkip(metadata6));
+
+    Assert.assertTrue(stringBetweenAnd.allSatisfy(metadata4));
+    Assert.assertTrue(stringBetweenAnd.allSatisfy(metadata5));
+    Assert.assertTrue(stringBetweenAnd.allSatisfy(metadata6));
   }
 
   @Test
@@ -257,6 +364,20 @@ public class StatisticsFilterTest {
     Assert.assertFalse(valueNotBetweenAnd.allSatisfy(metadata1));
     Assert.assertTrue(valueNotBetweenAnd.allSatisfy(metadata2));
     Assert.assertFalse(valueNotBetweenAnd.allSatisfy(metadata3));
+
+    Filter stringNotBetweenAnd =
+        ValueFilterApi.notBetween(
+            DEFAULT_MEASUREMENT_INDEX,
+            new Binary("b", STRING_CHARSET),
+            new Binary("c", STRING_CHARSET),
+            TSDataType.STRING);
+    Assert.assertFalse(stringNotBetweenAnd.canSkip(metadata4));
+    Assert.assertTrue(stringNotBetweenAnd.canSkip(metadata5));
+    Assert.assertFalse(stringNotBetweenAnd.canSkip(metadata6));
+
+    Assert.assertTrue(stringNotBetweenAnd.allSatisfy(metadata4));
+    Assert.assertFalse(stringNotBetweenAnd.allSatisfy(metadata5));
+    Assert.assertFalse(stringNotBetweenAnd.allSatisfy(metadata6));
   }
 
   @Test
@@ -264,7 +385,16 @@ public class StatisticsFilterTest {
     Filter valueIsNull = ValueFilterApi.isNull(DEFAULT_MEASUREMENT_INDEX);
 
     Assert.assertTrue(valueIsNull.canSkip(alignedMetadata1));
+    Assert.assertFalse(valueIsNull.canSkip(alignedMetadata2));
+    Assert.assertFalse(valueIsNull.canSkip(alignedMetadata3));
+    Assert.assertTrue(valueIsNull.canSkip(alignedMetadata4));
+    Assert.assertTrue(valueIsNull.canSkip(alignedMetadata5));
+
     Assert.assertFalse(valueIsNull.allSatisfy(alignedMetadata1));
+    Assert.assertFalse(valueIsNull.allSatisfy(alignedMetadata2));
+    Assert.assertTrue(valueIsNull.allSatisfy(alignedMetadata3));
+    Assert.assertFalse(valueIsNull.allSatisfy(alignedMetadata4));
+    Assert.assertFalse(valueIsNull.allSatisfy(alignedMetadata5));
   }
 
   @Test
@@ -274,10 +404,14 @@ public class StatisticsFilterTest {
     Assert.assertFalse(valueIsNotNull.canSkip(alignedMetadata1));
     Assert.assertFalse(valueIsNotNull.canSkip(alignedMetadata2));
     Assert.assertTrue(valueIsNotNull.canSkip(alignedMetadata3));
+    Assert.assertFalse(valueIsNotNull.canSkip(alignedMetadata4));
+    Assert.assertFalse(valueIsNotNull.canSkip(alignedMetadata5));
 
     Assert.assertTrue(valueIsNotNull.allSatisfy(alignedMetadata1));
     Assert.assertFalse(valueIsNotNull.allSatisfy(alignedMetadata2));
     Assert.assertFalse(valueIsNotNull.allSatisfy(alignedMetadata3));
+    Assert.assertTrue(valueIsNotNull.allSatisfy(alignedMetadata4));
+    Assert.assertTrue(valueIsNotNull.allSatisfy(alignedMetadata5));
   }
 
   @Test
@@ -320,6 +454,20 @@ public class StatisticsFilterTest {
     Assert.assertFalse(valueIn2.allSatisfy(metadata2));
     Assert.assertFalse(valueIn2.allSatisfy(metadata3));
     Assert.assertTrue(valueIn2.allSatisfy(alignedMetadata3));
+
+    Filter stringIn =
+        ValueFilterApi.in(
+            DEFAULT_MEASUREMENT_INDEX,
+            new HashSet<>(
+                Arrays.asList(new Binary("a", STRING_CHARSET), new Binary("b", STRING_CHARSET))),
+            TSDataType.STRING);
+    Assert.assertFalse(stringIn.canSkip(metadata4));
+    Assert.assertFalse(stringIn.canSkip(metadata5));
+    Assert.assertFalse(stringIn.canSkip(metadata6));
+
+    Assert.assertTrue(stringIn.allSatisfy(metadata4));
+    Assert.assertTrue(stringIn.allSatisfy(metadata5));
+    Assert.assertFalse(stringIn.allSatisfy(metadata6));
   }
 
   @Test
