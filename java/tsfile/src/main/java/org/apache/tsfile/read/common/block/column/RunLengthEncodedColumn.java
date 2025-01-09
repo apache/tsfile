496b5c192616de077ade8de1f51eb720cbd9fc90
Weihao Li <60659567+Wei-hao-Li@users.noreply.github.com>
2025-01-09 19:55:33 +0800

    Rollback method implement of RunLengthEncodedColumn

--- java/tsfile/src/main/java/org/apache/tsfile/read/common/block/column/RunLengthEncodedColumn.java
+++ java/tsfile/src/main/java/org/apache/tsfile/read/common/block/column/RunLengthEncodedColumn.java
@@ -120,6 +120,48 @@ public class RunLengthEncodedColumn implements Column {
   }
 
   @Override
+  public int[] getInts() {
+    int[] res = new int[positionCount];
+    Arrays.fill(res, value.getInt(0));
+    return res;
+  }
+
+  @Override
+  public long[] getLongs() {
+    long[] res = new long[positionCount];
+    Arrays.fill(res, value.getLong(0));
+    return res;
+  }
+
+  @Override
+  public float[] getFloats() {
+    float[] res = new float[positionCount];
+    Arrays.fill(res, value.getFloat(0));
+    return res;
+  }
+
+  @Override
+  public double[] getDoubles() {
+    double[] res = new double[positionCount];
+    Arrays.fill(res, value.getDouble(0));
+    return res;
+  }
+
+  @Override
+  public Binary[] getBinaries() {
+    Binary[] res = new Binary[positionCount];
+    Arrays.fill(res, value.getBinary(0));
+    return res;
+  }
+
+  @Override
+  public Object[] getObjects() {
+    Object[] res = new Object[positionCount];
+    Arrays.fill(res, value.getObject(0));
+    return res;
+  }
+
+  @Override
   public TsPrimitiveType getTsPrimitiveType(int position) {
     return value.getTsPrimitiveType(0);
   }
