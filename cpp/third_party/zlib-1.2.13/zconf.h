ae5b86e4a696bbda5b6773989dfd7fc7aec8a21f
ycycse <ycycse@gmail.com>
2025-05-07 21:00:39 +0800

    revert zconf

--- cpp/third_party/zlib-1.2.13/zconf.h
+++ cpp/third_party/zlib-1.2.13/zconf.h
@@ -1,7 +1,7 @@
 /* zconf.h -- configuration of the zlib compression library
-* Copyright (C) 1995-2016 Jean-loup Gailly, Mark Adler
-* For conditions of distribution and use, see copyright notice in zlib.h
-*/
+ * Copyright (C) 1995-2016 Jean-loup Gailly, Mark Adler
+ * For conditions of distribution and use, see copyright notice in zlib.h
+ */
 
 /* @(#) $Id$ */
 
@@ -9,11 +9,11 @@
 #define ZCONF_H
 
 /*
-* If you *really* need a unique prefix for all types and library functions,
-* compile with -DZ_PREFIX. The "standard" zlib should be compiled without it.
-* Even better than compiling with -DZ_PREFIX would be to use configure to set
-* this permanently in zconf.h using "./configure --zprefix".
-*/
+ * If you *really* need a unique prefix for all types and library functions,
+ * compile with -DZ_PREFIX. The "standard" zlib should be compiled without it.
+ * Even better than compiling with -DZ_PREFIX would be to use configure to set
+ * this permanently in zconf.h using "./configure --zprefix".
+ */
 #ifdef Z_PREFIX     /* may be set to #if 1 by ./configure */
 #  define Z_PREFIX_SET
 
@@ -191,9 +191,9 @@
 #endif
 
 /*
-* Compile with -DMAXSEG_64K if the alloc function cannot allocate more
-* than 64k bytes at a time (needed on systems with 16-bit int).
-*/
+ * Compile with -DMAXSEG_64K if the alloc function cannot allocate more
+ * than 64k bytes at a time (needed on systems with 16-bit int).
+ */
 #ifdef SYS16BIT
 #  define MAXSEG_64K
 #endif
@@ -241,16 +241,16 @@
 #endif
 
 #ifdef Z_SOLO
-typedef unsigned long z_size_t;
+   typedef unsigned long z_size_t;
 #else
 #  define z_longlong long long
 #  if defined(NO_SIZE_T)
-typedef unsigned NO_SIZE_T z_size_t;
+     typedef unsigned NO_SIZE_T z_size_t;
 #  elif defined(STDC)
 #    include <stddef.h>
-typedef size_t z_size_t;
+     typedef size_t z_size_t;
 #  else
-typedef unsigned long z_size_t;
+     typedef unsigned long z_size_t;
 #  endif
 #  undef z_longlong
 #endif
@@ -265,28 +265,28 @@ typedef unsigned long z_size_t;
 #endif
 
 /* Maximum value for windowBits in deflateInit2 and inflateInit2.
-* WARNING: reducing MAX_WBITS makes minigzip unable to extract .gz files
-* created by gzip. (Files created by minigzip can still be extracted by
-* gzip.)
-*/
+ * WARNING: reducing MAX_WBITS makes minigzip unable to extract .gz files
+ * created by gzip. (Files created by minigzip can still be extracted by
+ * gzip.)
+ */
 #ifndef MAX_WBITS
 #  define MAX_WBITS   15 /* 32K LZ77 window */
 #endif
 
 /* The memory requirements for deflate are (in bytes):
-           (1 << (windowBits+2)) +  (1 << (memLevel+9))
-that is: 128K for windowBits=15  +  128K for memLevel = 8  (default values)
-plus a few kilobytes for small objects. For example, if you want to reduce
-the default memory requirements from 256K to 128K, compile with
-    make CFLAGS="-O -DMAX_WBITS=14 -DMAX_MEM_LEVEL=7"
-Of course this will generally degrade compression (there's no free lunch).
+            (1 << (windowBits+2)) +  (1 << (memLevel+9))
+ that is: 128K for windowBits=15  +  128K for memLevel = 8  (default values)
+ plus a few kilobytes for small objects. For example, if you want to reduce
+ the default memory requirements from 256K to 128K, compile with
+     make CFLAGS="-O -DMAX_WBITS=14 -DMAX_MEM_LEVEL=7"
+ Of course this will generally degrade compression (there's no free lunch).
 
-  The memory requirements for inflate are (in bytes) 1 << windowBits
-that is, 32K for windowBits=15 (default value) plus about 7 kilobytes
-for small objects.
+   The memory requirements for inflate are (in bytes) 1 << windowBits
+ that is, 32K for windowBits=15 (default value) plus about 7 kilobytes
+ for small objects.
 */
 
-/* Type declarations */
+                        /* Type declarations */
 
 #ifndef OF /* function prototypes */
 #  ifdef STDC
@@ -305,14 +305,14 @@ for small objects.
 #endif
 
 /* The following definitions for FAR are needed only for MSDOS mixed
-* model programming (small or medium model with some far allocations).
-* This was tested only with MSC; for other MSDOS compilers you may have
-* to define NO_MEMCPY in zutil.h.  If you don't need the mixed model,
-* just define FAR to be empty.
-*/
+ * model programming (small or medium model with some far allocations).
+ * This was tested only with MSC; for other MSDOS compilers you may have
+ * to define NO_MEMCPY in zutil.h.  If you don't need the mixed model,
+ * just define FAR to be empty.
+ */
 #ifdef SYS16BIT
 #  if defined(M_I86SM) || defined(M_I86MM)
-/* MSC small or medium model */
+     /* MSC small or medium model */
 #    define SMALL_MEDIUM
 #    ifdef _MSC_VER
 #      define FAR _far
@@ -321,7 +321,7 @@ for small objects.
 #    endif
 #  endif
 #  if (defined(__SMALL__) || defined(__MEDIUM__))
-/* Turbo C small or medium model */
+     /* Turbo C small or medium model */
 #    define SMALL_MEDIUM
 #    ifdef __BORLANDC__
 #      define FAR _far
@@ -332,9 +332,9 @@ for small objects.
 #endif
 
 #if defined(WINDOWS) || defined(WIN32)
-/* If building or using zlib as a DLL, define ZLIB_DLL.
-   * This is not mandatory, but it offers a little performance increase.
-*/
+   /* If building or using zlib as a DLL, define ZLIB_DLL.
+    * This is not mandatory, but it offers a little performance increase.
+    */
 #  ifdef ZLIB_DLL
 #    if defined(WIN32) && (!defined(__BORLANDC__) || (__BORLANDC__ >= 0x500))
 #      ifdef ZLIB_INTERNAL
@@ -344,10 +344,10 @@ for small objects.
 #      endif
 #    endif
 #  endif  /* ZLIB_DLL */
-      /* If building or using zlib with the WINAPI/WINAPIV calling convention,
-   * define ZLIB_WINAPI.
-   * Caution: the standard ZLIB1.DLL is NOT compiled using ZLIB_WINAPI.
-       */
+   /* If building or using zlib with the WINAPI/WINAPIV calling convention,
+    * define ZLIB_WINAPI.
+    * Caution: the standard ZLIB1.DLL is NOT compiled using ZLIB_WINAPI.
+    */
 #  ifdef ZLIB_WINAPI
 #    ifdef FAR
 #      undef FAR
@@ -356,8 +356,8 @@ for small objects.
 #      define WIN32_LEAN_AND_MEAN
 #    endif
 #    include <windows.h>
-/* No need for _export, use ZLIB.DEF instead. */
-/* For complete Windows compatibility, use WINAPI, not __stdcall. */
+     /* No need for _export, use ZLIB.DEF instead. */
+     /* For complete Windows compatibility, use WINAPI, not __stdcall. */
 #    define ZEXPORT WINAPI
 #    ifdef WIN32
 #      define ZEXPORTVA WINAPIV
@@ -400,10 +400,10 @@ typedef unsigned int   uInt;  /* 16 bits or more */
 typedef unsigned long  uLong; /* 32 bits or more */
 
 #ifdef SMALL_MEDIUM
-/* Borland C/C++ and some old MSC versions ignore FAR inside typedef */
+   /* Borland C/C++ and some old MSC versions ignore FAR inside typedef */
 #  define Bytef Byte FAR
 #else
-typedef Byte  FAR Bytef;
+   typedef Byte  FAR Bytef;
 #endif
 typedef char  FAR charf;
 typedef int   FAR intf;
@@ -411,13 +411,13 @@ typedef uInt  FAR uIntf;
 typedef uLong FAR uLongf;
 
 #ifdef STDC
-typedef void const *voidpc;
-typedef void FAR   *voidpf;
-typedef void       *voidp;
+   typedef void const *voidpc;
+   typedef void FAR   *voidpf;
+   typedef void       *voidp;
 #else
-typedef Byte const *voidpc;
-typedef Byte FAR   *voidpf;
-typedef Byte       *voidp;
+   typedef Byte const *voidpc;
+   typedef Byte FAR   *voidpf;
+   typedef Byte       *voidp;
 #endif
 
 #if !defined(Z_U4) && !defined(Z_SOLO) && defined(STDC)
@@ -432,9 +432,9 @@ typedef Byte       *voidp;
 #endif
 
 #ifdef Z_U4
-typedef Z_U4 z_crc_t;
+   typedef Z_U4 z_crc_t;
 #else
-typedef unsigned long z_crc_t;
+   typedef unsigned long z_crc_t;
 #endif
 
 #ifdef HAVE_UNISTD_H    /* may be set to #if 1 by ./configure */
@@ -464,11 +464,11 @@ typedef unsigned long z_crc_t;
 #endif
 
 /* a little trick to accommodate both "#define _LARGEFILE64_SOURCE" and
-* "#define _LARGEFILE64_SOURCE 1" as requesting 64-bit operations, (even
-* though the former does not conform to the LFS document), but considering
-* both "#undef _LARGEFILE64_SOURCE" and "#define _LARGEFILE64_SOURCE 0" as
-* equivalently requesting no 64-bit operations
-*/
+ * "#define _LARGEFILE64_SOURCE 1" as requesting 64-bit operations, (even
+ * though the former does not conform to the LFS document), but considering
+ * both "#undef _LARGEFILE64_SOURCE" and "#define _LARGEFILE64_SOURCE 0" as
+ * equivalently requesting no 64-bit operations
+ */
 #if defined(_LARGEFILE64_SOURCE) && -_LARGEFILE64_SOURCE - -1 == 1
 #  undef _LARGEFILE64_SOURCE
 #endif
@@ -529,19 +529,19 @@ typedef unsigned long z_crc_t;
 
 /* MVS linker does not support external names larger than 8 bytes */
 #if defined(__MVS__)
-#pragma map(deflateInit_,"DEIN")
-#pragma map(deflateInit2_,"DEIN2")
-#pragma map(deflateEnd,"DEEND")
-#pragma map(deflateBound,"DEBND")
-#pragma map(inflateInit_,"ININ")
-#pragma map(inflateInit2_,"ININ2")
-#pragma map(inflateEnd,"INEND")
-#pragma map(inflateSync,"INSY")
-#pragma map(inflateSetDictionary,"INSEDI")
-#pragma map(compressBound,"CMBND")
-#pragma map(inflate_table,"INTABL")
-#pragma map(inflate_fast,"INFA")
-#pragma map(inflate_copyright,"INCOPY")
+  #pragma map(deflateInit_,"DEIN")
+  #pragma map(deflateInit2_,"DEIN2")
+  #pragma map(deflateEnd,"DEEND")
+  #pragma map(deflateBound,"DEBND")
+  #pragma map(inflateInit_,"ININ")
+  #pragma map(inflateInit2_,"ININ2")
+  #pragma map(inflateEnd,"INEND")
+  #pragma map(inflateSync,"INSY")
+  #pragma map(inflateSetDictionary,"INSEDI")
+  #pragma map(compressBound,"CMBND")
+  #pragma map(inflate_table,"INTABL")
+  #pragma map(inflate_fast,"INFA")
+  #pragma map(inflate_copyright,"INCOPY")
 #endif
 
 #endif /* ZCONF_H */
