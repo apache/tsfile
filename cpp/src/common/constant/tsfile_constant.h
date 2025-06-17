c40d043a62c3e4b58b6e47e6eb4d5ddc17900a95
ColinLee <shuolin_l@163.com>
2025-06-18 01:07:07 +0800

    add no_str_to_read.

--- cpp/src/common/constant/tsfile_constant.h
+++ cpp/src/common/constant/tsfile_constant.h
@@ -41,7 +41,7 @@ namespace storage
     static const unsigned char VALUE_COLUMN_MASK = 0x40;
 
     static const std::string TIME_COLUMN_ID = "";
-    static const int NO_STR_TO_READ = INT32_MAX;
+    static const int NO_STR_TO_READ = -1;
  
     static const std::regex IDENTIFIER_PATTERN("([a-zA-Z0-9_\\u2E80-\\u9FFF]+)");
     static const std::regex NODE_NAME_PATTERN("(\\*{0,2}[a-zA-Z0-9_\\u2E80-\\u9FFF]+\\*{0,2})");
