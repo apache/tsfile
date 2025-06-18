fc209ad1535c2a36453091d9b785cfcd05b3e76d
761417898 <761417898@qq.com>
2025-06-18 16:42:38 +0800

    Revert "Resolved case sensitivity issue when reading column names. (#517)"
    This reverts commit c501bf4e0ac13cdbe0b7d404cd4ea3ef99904203.

--- cpp/test/cwrapper/c_release_test.cc
+++ cpp/test/cwrapper/c_release_test.cc
@@ -307,9 +307,9 @@ TEST_F(CReleaseTest, TsFileWriterMultiDataType) {
         ASSERT_EQ("device1", std::string(str_value));
         free(str_value);
         ASSERT_EQ(value, tsfile_result_set_get_value_by_name_int32_t(result_set,
-                                                                     "INT32"));
+                                                                     "int32"));
         ASSERT_EQ(value * 100, tsfile_result_set_get_value_by_name_int64_t(
-                                   result_set, "INT64"));
+                                   result_set, "int64"));
         ASSERT_EQ(value * 100.0, tsfile_result_set_get_value_by_name_float(
                                      result_set, "FLOAT"));
 
