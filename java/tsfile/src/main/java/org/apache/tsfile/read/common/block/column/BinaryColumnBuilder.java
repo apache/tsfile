538b4aa204185a135a89ce37ca22edb5d4df3a05
Jackie Tien <jackietien97@gmail.com>
2025-07-03 11:46:36 +0800

    [To dev/1.1] Correct the memroy calculation of BinaryColumnBuilder

--- java/tsfile/src/main/java/org/apache/tsfile/read/common/block/column/BinaryColumnBuilder.java
+++ java/tsfile/src/main/java/org/apache/tsfile/read/common/block/column/BinaryColumnBuilder.java
@@ -72,6 +72,11 @@ public class BinaryColumnBuilder implements ColumnBuilder {
 
     hasNonNullValue = true;
     positionCount++;
+    if (columnBuilderStatus != null) {
+      columnBuilderStatus.addBytes(
+          BinaryColumn.SHALLOW_SIZE_IN_BYTES_PER_POSITION
+              + (value == null ? 0 : (int) value.ramBytesUsed()));
+    }
     return this;
   }
 
@@ -105,6 +110,9 @@ public class BinaryColumnBuilder implements ColumnBuilder {
 
     hasNullValue = true;
     positionCount++;
+    if (columnBuilderStatus != null) {
+      columnBuilderStatus.addBytes(BinaryColumn.SHALLOW_SIZE_IN_BYTES_PER_POSITION);
+    }
     return this;
   }
 
