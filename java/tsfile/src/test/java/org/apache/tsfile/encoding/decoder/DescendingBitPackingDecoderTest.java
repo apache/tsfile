3f2cce5c8513a664eedbc8ce01cfb3801ddef546
Mr_Spade <Mr_Spade@foxmail.com>
2025-09-30 13:45:49 +0800

    update test (give up compile)

--- java/tsfile/src/test/java/org/apache/tsfile/encoding/decoder/DescendingBitPackingDecoderTest.java
+++ java/tsfile/src/test/java/org/apache/tsfile/encoding/decoder/DescendingBitPackingDecoderTest.java
@@ -20,7 +20,7 @@
 package org.apache.tsfile.encoding.decoder;
 
 import org.apache.tsfile.encoding.encoder.DescendingBitPackingEncoder;
-import org.apache.tsfile.encoding.encoder.SeparateStorageEncoder;
+import org.apache.tsfile.encoding.encoder.Encoder;
 
 import org.junit.Test;
 
@@ -30,68 +30,49 @@ import java.nio.ByteBuffer;
 import static org.junit.Assert.assertEquals;
 
 public class DescendingBitPackingDecoderTest {
+  protected static long[] getTestData() {
+    return new long[] {
+      0,
+      -1,
+      1,
+      -2,
+      2,
+      0,
+      0,
+      -3,
+      3,
+      0,
+      -4,
+      0,
+      0,
+      0,
+      4,
+      -5,
+      5,
+      Long.MIN_VALUE,
+      Long.MAX_VALUE,
+      Long.MAX_VALUE - 1,
+      Long.MIN_VALUE + 1,
+      0,
+      0
+    };
+  }
+
   @Test
   public void test() throws Exception {
-    long[] original =
-        new long[] {
-          0,
-          -1,
-          1,
-          -2,
-          2,
-          0,
-          0,
-          -3,
-          3,
-          0,
-          -4,
-          0,
-          0,
-          0,
-          4,
-          -5,
-          5,
-          Long.MIN_VALUE,
-          Long.MAX_VALUE,
-          Long.MAX_VALUE - 1,
-          Long.MIN_VALUE + 1,
-          0,
-          0
-        };
-    compressDecompressAndAssert(original);
-    compressDecompressAndAssertSeparateStorage(original);
+    long[] original = getTestData();
+    compressDecompressAndAssert(
+        original, new DescendingBitPackingEncoder(), new DescendingBitPackingDecoder());
   }
 
-  private static void compressDecompressAndAssert(long[] original) throws Exception {
-    DescendingBitPackingEncoder encoder = new DescendingBitPackingEncoder();
+  protected static void compressDecompressAndAssert(
+      long[] original, Encoder encoder, Decoder decoder) throws Exception {
     ByteArrayOutputStream bout = new ByteArrayOutputStream();
     for (long v : original) {
       encoder.encode(v, bout);
     }
     encoder.flush(bout);
     // Decode and verify
-    DescendingBitPackingDecoder decoder = new DescendingBitPackingDecoder();
-    ByteBuffer buffer = ByteBuffer.wrap(bout.toByteArray());
-
-    int i = 0;
-    while (decoder.hasNext(buffer)) {
-      long actual = decoder.readLong(buffer);
-      long expected = original[i];
-      assertEquals("Mismatch at index " + i, expected, actual);
-      i++;
-    }
-    assertEquals(original.length, i);
-  }
-
-  private static void compressDecompressAndAssertSeparateStorage(long[] original) throws Exception {
-    SeparateStorageEncoder encoder = new SeparateStorageEncoder();
-    ByteArrayOutputStream bout = new ByteArrayOutputStream();
-    for (long v : original) {
-      encoder.encode(v, bout);
-    }
-    encoder.flush(bout);
-    // Decode and verify
-    SeparateStorageDecoder decoder = new SeparateStorageDecoder();
     ByteBuffer buffer = ByteBuffer.wrap(bout.toByteArray());
 
     int i = 0;
