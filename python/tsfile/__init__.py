0ee0fa0458e327de6af51af710387a28eb20018f
ColinLee <shuolin_l@163.com>
2025-10-20 15:12:51 +0800

    Add workflow for python release.

--- python/tsfile/__init__.py
+++ python/tsfile/__init__.py
@@ -19,10 +19,6 @@
 import ctypes
 import os
 import platform
-system = platform.system()
-if system == "Windows":
-    ctypes.WinDLL(os.path.join(os.path.dirname(__file__), "libtsfile.dll"), winmode=0)
-
 from .constants import *
 from .schema import *
 from .row_record import *
