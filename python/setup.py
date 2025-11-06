44c4d1f26978c4dc1c58edf847e3fd30be21720e
shuolin_l <shuolin_l@163.com>
2025-11-06 15:13:12 +0800

    bump version .

--- python/setup.py
+++ python/setup.py
@@ -33,7 +33,7 @@ CPP_OUT = ROOT / ".." / "cpp" / "target" / "build"
 CPP_LIB = CPP_OUT / "lib"
 CPP_INC = CPP_OUT / "include"
 
-version = "2.1.2"
+version = "2.2.0.dev"
 system = platform.system()
 
 (PKG / "include").mkdir(exist_ok=True)
