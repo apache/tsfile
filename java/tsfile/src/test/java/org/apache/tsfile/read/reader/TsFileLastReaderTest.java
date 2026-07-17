b8a355f16d278ab31b0a7897d5b58e57e4ba7499
Tian Jiang <jt2594838@163.com>
2026-07-17 17:58:36 +0800

    refactor TsFileLastReaderTest

--- java/tsfile/src/test/java/org/apache/tsfile/read/reader/TsFileLastReaderTest.java
+++ java/tsfile/src/test/java/org/apache/tsfile/read/reader/TsFileLastReaderTest.java
@@ -25,6 +25,7 @@ import org.apache.tsfile.file.metadata.IDeviceID;
 import org.apache.tsfile.file.metadata.IDeviceID.Factory;
 import org.apache.tsfile.read.TimeValuePair;
 import org.apache.tsfile.read.common.type.Type;
+import org.apache.tsfile.read.common.type.service.TypeService;
 import org.apache.tsfile.utils.Binary;
 import org.apache.tsfile.utils.Pair;
 import org.apache.tsfile.utils.TsPrimitiveType;
@@ -62,6 +63,34 @@ public class TsFileLastReaderTest {
       Arrays.asList(TSDataType.INT64, TSDataType.BLOB);
   private static final Map<TSDataType, TabletAddValueFunction> typeAddValueFunctions =
       new HashMap<>();
+  private static final TypeService<OffsetTabletAddValueFunction> ADD_OFFSET_VALUE_SERVICE =
+      type ->
+          switch (type.getTypeEnum()) {
+            case INT64 ->
+                (tablet, row, column, offset) -> tablet.addValue(row, column, (long) row + offset);
+            case BLOB ->
+                (tablet, row, column, offset) ->
+                    tablet.addValue(
+                        row,
+                        column,
+                        Long.toBinaryString(row + offset).getBytes(StandardCharsets.UTF_8));
+            case BOOLEAN,
+                DATE,
+                DOUBLE,
+                FLOAT,
+                INT32,
+                OBJECT,
+                ROW,
+                STRING,
+                TEXT,
+                TIMESTAMP,
+                UNKNOWN,
+                VECTOR ->
+                (tablet, row, column, offset) -> {
+                  throw new IllegalArgumentException(
+                      "Unsupported TSDataType " + type.getTypeEnum());
+                };
+          };
 
   static {
     typeAddValueFunctions.put(
@@ -71,6 +100,7 @@ public class TsFileLastReaderTest {
         ((tablet, row, column) ->
             tablet.addValue(
                 row, column, Long.toBinaryString(row).getBytes(StandardCharsets.UTF_8))));
+    ADD_OFFSET_VALUE_SERVICE.check();
   }
 
   private final String filePath = "target/test.tsfile";
@@ -143,25 +173,21 @@ public class TsFileLastReaderTest {
         }
         for (int j = 0; j < measurementNum / 2; j++) {
           TSDataType tsDataType = dataTypes.get(j % dataTypes.size());
+          OffsetTabletAddValueFunction addValueFunction =
+              ADD_OFFSET_VALUE_SERVICE.call(Type.fromTsDataType(tsDataType));
           for (int k = 0; k < seriesPointNum; k++) {
-            switch (tsDataType) {
-              case INT64:
-                tablet.addValue(k, j, (long) k + seriesPointNum / 2);
-                break;
-              case BLOB:
-                tablet.addValue(
-                    k,
-                    j,
-                    Long.toBinaryString(k + seriesPointNum / 2).getBytes(StandardCharsets.UTF_8));
-                break;
-              default:
-                throw new IllegalArgumentException("Unsupported TSDataType " + tsDataType);
-            }
+            addValueFunction.addValue(tablet, k, j, seriesPointNum / 2);
           }
         }
         writer.writeTree(tablet);
       }
     }
+  }
+
+  @FunctionalInterface
+  private interface OffsetTabletAddValueFunction {
+
+    void addValue(Tablet tablet, int row, int column, int offset);
   }
 
   private void createAlignedFileWithObject(int seriesPointNum)
